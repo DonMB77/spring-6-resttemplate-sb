624ba4b11c5503670ad8a1972cfe0353302b9873
DonMB77 <purposefuldrifter@gmail.com>
2025-01-17 17:17:29 +0100

    BeerClient.deleteBeer() impl + test

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -6,8 +6,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.math.BigDecimal;
+import java.net.HttpRetryException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +18,25 @@ class BeerClientImplTest {
 
     @Autowired
     BeerClientImpl beerClient;
+
+    @Test
+    void testDeleteBeer() {
+        BeerDTO newDto = BeerDTO.builder()
+                .price(new BigDecimal("10.99"))
+                .beerName("Mango Bobs 2")
+                .beerStyle(BeerStyle.IPA)
+                .quantityOnHand(500)
+                .upc("123245")
+                .build();
+
+        BeerDTO beerDTO = beerClient.createBeer(newDto);
+
+        beerClient.deleteBeer(beerDTO.getId());
+
+        assertThrows(HttpClientErrorException.class,() -> {
+           beerClient.getBeerById(beerDTO.getId());
+        });
+    }
 
     @Test
     void testUpdateBeer() {
