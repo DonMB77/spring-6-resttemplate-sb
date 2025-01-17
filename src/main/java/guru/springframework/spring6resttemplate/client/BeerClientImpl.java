72849a69b9fec67d16f3ee01f6c1a0f2728720b1
DonMB77 <purposefuldrifter@gmail.com>
2025-01-17 20:49:53 +0100

    BeerClientMockTest testListBeersWithQueryParam()

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -38,19 +38,19 @@ public class BeerClientImpl implements BeerClient {
             uriComponentsBuilder.queryParam("beerName", beerName);
         }
 
-        if (beerName != null) {
+        if (beerStyle != null) {
             uriComponentsBuilder.queryParam("beerStyle", beerStyle);
         }
 
-        if (beerName != null) {
+        if (showInventory != null) {
             uriComponentsBuilder.queryParam("showInventory", showInventory);
         }
 
-        if (beerName != null) {
+        if (pageNumber != null) {
             uriComponentsBuilder.queryParam("pageNumber", pageNumber);
         }
 
-        if (beerName != null) {
+        if (pageSize != null) {
             uriComponentsBuilder.queryParam("pageSize", pageSize);
         }
 
